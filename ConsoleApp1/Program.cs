27e466920ef4d2f1a0653ac44aa2b02fbe196c50
Alexsorokin40 <alexsorokin40@gmail.com>
2022-03-29 11:16:21 +0300

    Discussion1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int    maxSpeed;
+            float  indexMap;
+            ulong  countScore;
+            byte   indificator;
+            long   sum;
+            string textLine;
+            int    idPlayer;
+            int    RectangleX;
+            int    RectangleY;
+            bool   flagF;
+        }
+    }
+}
